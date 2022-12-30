5dbee2a46cfe2cac48d9d7d85f4537142486b567
Karan Raj <97748755+karanraj007aA@users.noreply.github.com>
2022-12-30 19:36:07 +0530

    web_code_added

---
+++ App.js
@@ -0,0 +1,42 @@
+// import logo from './logo.svg';
+// eslint-disable-next-line
+import "./App.css";
+// eslint-disable-next-line
+// import HooksCounter from './HooksCounter';
+// import abc from './abc';
+// import HooksUsingPrevCounter from './HooksUsingPrevCounter';
+function App() {
+  return (
+    <>
+      <div className="container-center-horizontal">
+        <div className="navbar-tablet screen">
+          <img
+            className="group-1"
+            src="https://cdn.animaapp.com/projects/63adb45c9e2b3ec4b4723d69/releases/63aeb697634d07ad4c78e34d/img/group-1@2x.png"
+            alt="Group 1"
+          />
+          <img
+            className="paytm_logo-1"
+            src="https://cdn.animaapp.com/projects/63adb45c9e2b3ec4b4723d69/releases/63aeb697634d07ad4c78e34d/img/paytm-logo-1@2x.png"
+            alt="paytm_logo 1"
+          />
+
+          <div className="group-2">
+            <div className="overlap-group">
+              <div className="sign-in">
+                <div className="new Sign">Sign in</div>
+                <img
+                  className="group"
+                  src="https://cdn.animaapp.com/projects/63adb45c9e2b3ec4b4723d69/releases/63aeb697634d07ad4c78e34d/img/group@2x.png"
+                  alt="Group"
+                ></img>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
